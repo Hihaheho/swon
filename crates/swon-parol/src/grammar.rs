44a97447e816110c70e97d1503ac2c9f063b5f46
Ryo Hirayama <ryo12redstone@gmail.com>
2024-08-25 14:44:30 +0900

    Make the grammar valid LL1

---
+++ crates/swon-parol/src/grammar.rs
@@ -0,0 +1,44 @@
+use crate::grammar_trait::{GrammarTrait, Swon};
+#[allow(unused_imports)]
+use parol_runtime::{Result, Token};
+use std::fmt::{Debug, Display, Error, Formatter};
+
+///
+/// Data structure that implements the semantic actions for our Swon grammar
+/// !Change this type as needed!
+///
+#[derive(Debug, Default)]
+pub struct Grammar<'t> {
+    pub swon: Option<Swon<'t>>,
+}
+
+impl Grammar<'_> {
+    pub fn new() -> Self {
+        Grammar::default()
+    }
+}
+
+impl Display for Swon<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::result::Result<(), Error> {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl Display for Grammar<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::result::Result<(), Error> {
+        match &self.swon {
+            Some(swon) => writeln!(f, "{}", swon),
+            None => write!(f, "No parse result"),
+        }
+    }
+}
+
+impl<'t> GrammarTrait<'t> for Grammar<'t> {
+    // !Adjust your implementation as needed!
+
+    /// Semantic action for non-terminal 'Swon'
+    fn swon(&mut self, arg: &Swon<'t>) -> Result<()> {
+        self.swon = Some(arg.clone());
+        Ok(())
+    }
+}
--- docs/extensions.md
+++ docs/extensions.md
@@ -1,17 +1,21 @@
 # Extensions
 
 Extensions are used to provide additional information without affecting the data. `$` is the prefix of the extension namespace. If you want to use a key that is prefixed with `$`, you must quote it as `"$key"`.
+Extensions should be placed before any other bindings.
 
 ## $swon
 
 ```swon
 @ $swon
+# version of the SWON specification
 version = "https://swon.dev/v1"
+# schema of this SWON file
 schema = "https://swon.dev/schemas/swon-schema/v1"
 ```
 
 - `$swon-lint.config = "https://swon.dev/default-configs/swon-lint/v1"`
 - `$swon-fmt.config = "https://swon.dev/default-configs/swon-fmt/v1"`
+- `$root = "script"` to indicate the root key of this section
 
 ## $variant
 
